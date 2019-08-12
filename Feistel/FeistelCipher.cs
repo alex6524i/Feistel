4bf0b69683ed8e2bb29cc1fcc5d27e11e30a7970
alex6524i <alex6524i@gmail.com>
2019-08-12 22:21:49 +0300

    Add project files.

---
+++ Feistel/FeistelCipher.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Feistel
+{
+    class FeistelCipher
+    {
+        byte[] _key;
+        int _blockSize;
+        FeistelType _feistelType;
+        CipherMode _cipherMode;
+        byte[] _IVcbc;
+        byte[] _IVcfb;
+        public IEnumerable<byte[]> _keys
+        {
+            get
+            {
+                int i;
+                for (i = 0; i < 4; i++)
+                {
+                    yield return CycleShift(_key, (byte)(i * 7), Direction.Right);
+                }
+            }
+        }
+        public IEnumerable<byte[]> _keysReverse
+        {
+            get
+            {
+                return _keys.Reverse();
+            }
+        }
+
+        public FeistelCipher (int keySize, FeistelType feistelType, CipherMode cipherMode)
+        {
+            _feistelType = feistelType;
+            _cipherMode = cipherMode;
+            _key = GenerateKey(keySize);
+            _blockSize = _key.Length * ((feistelType == FeistelType.ClassicFeistel) ? 2 : 4);
+            if(cipherMode == CipherMode.CBC)
+            {
+                _IVcbc = GenerateIV(_blockSize);
+            }
+            if(cipherMode == CipherMode.CFB)
+            {
+                _IVcfb = GenerateIV(_blockSize);
+            }
+        }
+        public FeistelCipher(byte[] key, FeistelType feistelType, CipherMode cipherMode):this(key.Length, feistelType, cipherMode)
+        {
+            if(key.Length % 2 != 0)
+            {
+                throw new Exception("Key size must be multiple of 2 size!");
+            }
+            _key = key;
+            _blockSize = key.Length * ((feistelType == FeistelType.ClassicFeistel) ? 2 : 4);
+            if (cipherMode == CipherMode.CBC)
+            {
+                _IVcbc = GenerateIV(_blockSize);
+            }
+            if (cipherMode == CipherMode.CFB)
+            {
+                _IVcfb = GenerateIV(_blockSize * 4);
+            }
+        }
+
+        public string CryptText(string plainText, CryptType cryptType)
+        {
+            while (plainText.Length % _blockSize != 0)
+                plainText += new char();
+
+            var roundKeys = (_cipherMode == CipherMode.CFB) ? _keys : (cryptType == CryptType.Encrypt) ? _keys : _keysReverse;
+
+            var plainTextBytes = Encoding.Default.GetBytes(plainText);
+
+            byte[] iv = new byte[_blockSize];
+            byte[] ivNew = new byte[_blockSize];
+
+            if (_cipherMode == CipherMode.CBC)
+            {
+                iv = _IVcbc;
+            }
+            else if(_cipherMode == CipherMode.CFB)
+            {
+                iv = _IVcfb;
+            }
+
+            var result = "";
+            var blockPosition = 0;
+
+            while (blockPosition < plainTextBytes.Length)
+            {
+                var blockBytes = new byte[_blockSize];
+                Array.Copy(plainTextBytes, blockPosition, blockBytes, 0, _blockSize);
+                if (_cipherMode == CipherMode.None)
+                    blockBytes = CryptBlock(blockBytes, roundKeys, cryptType);
+                else if (_cipherMode == CipherMode.CBC)
+                    blockBytes = CryptBlockCBC(blockBytes, ref iv, ref ivNew, roundKeys, cryptType);
+                else if (_cipherMode == CipherMode.CFB)
+                    blockBytes = CryptBlockCFB(blockBytes, ref iv, roundKeys, cryptType);
+                else
+                    throw new Exception("Undefined cipher mode");
+
+                result += Encoding.Default.GetString(blockBytes);
+                blockPosition += _blockSize;
+            }
+            return result.Trim('\0');
+        }
+        byte[] CryptBlock(byte[] blockBytes, IEnumerable<byte[]> roundKeys, CryptType cryptType)
+        {
+            foreach (var key in roundKeys)
+            {
+                if (_feistelType == FeistelType.ClassicFeistel)
+                    blockBytes = PerformEncryptionClassic(blockBytes, key, key.SequenceEqual(roundKeys.Last()));
+                else
+                    blockBytes = PerformEncryptionVariant1(blockBytes, key, cryptType);
+            }
+            return blockBytes;
+        }
+        byte[] CryptBlockCBC(byte[] blockBytes, ref byte[] iv, ref byte[] ivNew, IEnumerable<byte[]> roundKeys, CryptType cryptType)
+        {
+            if (cryptType == CryptType.Encrypt)
+                blockBytes = XOR(blockBytes, iv);
+            else
+                Array.Copy(blockBytes, ivNew, blockBytes.Length);
+            foreach (var key in roundKeys)
+            {
+                if (_feistelType == FeistelType.ClassicFeistel)
+                    blockBytes = PerformEncryptionClassic(blockBytes, key, key.SequenceEqual(roundKeys.Last()));
+                else
+                    blockBytes = PerformEncryptionVariant1(blockBytes, key, cryptType);
+            }
+            if (cryptType == CryptType.Encrypt)
+                iv = blockBytes;
+            else
+            {
+                blockBytes = XOR(blockBytes, iv);
+                Array.Copy(ivNew, iv, ivNew.Length);
+            }
+            return blockBytes;
+        }
+        byte[] CryptBlockCFB(byte[] blockBytes, ref byte[] iv, IEnumerable<byte[]> roundKeys, CryptType cryptType)
+        {
+            var tempCipher = new byte[blockBytes.Length];
+            Array.Copy(blockBytes, tempCipher, blockBytes.Length);
+            foreach(var key in roundKeys)
+            {
+                if (_feistelType == FeistelType.ClassicFeistel)
+                    iv = PerformEncryptionClassic(iv, key, key.SequenceEqual(roundKeys.Last()));
+                else
+                    iv = PerformEncryptionVariant1(iv, key, CryptType.Encrypt);
+            }
+            var result = XOR(iv, blockBytes);
+            Array.Copy((cryptType == CryptType.Encrypt) ? result : tempCipher, iv, result.Length);
+            return result;
+        }
+        byte[] PerformEncryptionClassic(byte[] blockBytes, byte[] roundKey, bool lastRound)
+        {
+            byte[] leftBlock = new byte[blockBytes.Length / 2];
+            byte[] rightBlock = new byte[blockBytes.Length / 2];
+
+            Array.Copy(blockBytes, leftBlock, leftBlock.Length);
+            Array.Copy(blockBytes, leftBlock.Length, rightBlock, 0, rightBlock.Length);
+
+            byte[] rightBlockNew = ApplyRoundFunction(leftBlock, roundKey);
+
+            for (uint rightBlockNo = 0; rightBlockNo < leftBlock.Length; ++rightBlockNo)
+            {
+                rightBlockNew[rightBlockNo] ^= rightBlock[rightBlockNo];
+            }
+
+            byte[] resultBlockBytes = new byte[blockBytes.Length];
+
+            if (!lastRound)
+            {
+                Array.Copy(rightBlockNew, resultBlockBytes, rightBlockNew.Length);
+                Array.Copy(leftBlock, 0, resultBlockBytes, leftBlock.Length, rightBlockNew.Length);
+            }
+            else
+            {
+                Array.Copy(leftBlock, resultBlockBytes, leftBlock.Length);
+                Array.Copy(rightBlockNew, 0, resultBlockBytes, leftBlock.Length, rightBlockNew.Length);
+            }
+            return resultBlockBytes;
+        }
+        byte[] PerformEncryptionVariant1(byte[] block, byte[] roundKey, CryptType cryptType)
+        {
+            byte[] q1 = new byte[block.Length / 4];
+            byte[] q2 = new byte[block.Length / 4];
+            byte[] q3 = new byte[block.Length / 4];
+            byte[] q4 = new byte[block.Length / 4];
+
+            Array.Copy(block, q1, q1.Length);
+            Array.Copy(block, q1.Length, q2, 0, q2.Length);
+            Array.Copy(block, q1.Length + q2.Length, q3, 0, q3.Length);
+            Array.Copy(block, q1.Length + q2.Length + q3.Length, q4, 0, q4.Length);
+
+            var q2new = new byte[q2.Length];
+            if (cryptType == CryptType.Encrypt)
+            {
+                q2new = ApplyRoundFunction(q1, roundKey);
+                q2new = XOR(q2new, q2);
+            }
+            else
+            {
+                q2new = ApplyRoundFunction(q4, roundKey);
+                q2new = XOR(q2new, q1);
+            }
+
+            byte[] result = new byte[block.Length];
+
+            if (cryptType == CryptType.Encrypt)
+            {
+                Array.Copy(q2new, result, q2new.Length);
+                Array.Copy(q3, 0, result, q2new.Length, q3.Length);
+                Array.Copy(q4, 0, result, q2new.Length + q3.Length, q4.Length);
+                Array.Copy(q1, 0, result, q2new.Length + q3.Length + q4.Length, q1.Length);
+            }
+            else
+            {
+                Array.Copy(q4, result, q4.Length);
+                Array.Copy(q2new, 0, result, q4.Length, q2new.Length);
+                Array.Copy(q2, 0, result, q4.Length + q2new.Length, q2.Length);
+                Array.Copy(q3, 0, result, q4.Length + q1.Length + q2new.Length, q3.Length);
+            }
+            return result;
+        }
+        byte[] ApplyRoundFunction(byte[] blockBytes, byte[] roundKey)
+        {
+            return XOR(CycleShift(blockBytes, 9, Direction.Left), Inverse(MultiplicationMod2(CycleShift(roundKey, 11, Direction.Right), blockBytes)));
+        }
+
+        byte[] MultiplicationMod2(byte[] array1, byte[] array2)
+        {
+            var result = new byte[array1.Length];
+            for(uint i = 0; i < result.Length; i++)
+            {
+                result[i] = (byte)((array1[i] + array2[i]) % (Math.Pow(2, _blockSize) + 1));
+            }
+            return result;
+        }
+        byte[] XOR(byte[] array1, byte[] array2)
+        {
+            var result = new byte[array1.Length];
+            for(uint i = 0; i < result.Length; i++)
+            {
+                result[i] = (byte)(array1[i] ^ array2[i]);
+            }
+            return result;
+        }
+        byte[] Inverse(byte[] array)
+        {
+            var result = new byte[array.Length];
+            for(uint i = 0; i < result.Length; i++)
+            {
+                result[i] = (byte)~array[i];
+            }
+            return result;
+        }
+        byte[] Shift(byte[] array, byte count, Direction direction)
+        {
+            var result = new byte[array.Length];
+            Array.Copy(array, result, array.Length);
+            while(count > 0)
+            {
+                result = PerformShift(result, direction);
+                count--;
+            }
+            return result;
+        }
+        byte[] PerformShift(byte[] array, Direction direction)
+        {
+            var result = new byte[array.Length];
+            Array.Copy(array, result, array.Length);
+            if(direction == Direction.Left)
+            {
+                bool tempByte = false;
+                bool newTempByte;
+                for(int i = result.Length - 1; i >= 0; i--)
+                {
+                    newTempByte = GetBit(result[i], 7);
+                    result[i] = (byte)(result[i] << 1);
+                    SetBit(tempByte, ref result[i], 0);
+                    tempByte = newTempByte;
+                }
+            }
+            else
+            {
+                bool tempByte = false;
+                bool newTempByte;
+                for(int i = 0; i < result.Length; i++)
+                {
+                    newTempByte = GetBit(result[i], 0);
+                    result[i] = (byte)(result[i] >> 1);
+                    SetBit(tempByte, ref result[i], 7);
+                    tempByte = newTempByte;
+                }
+            }
+            return result;
+        }
+        byte[] CycleShift(byte[] array, byte count, Direction direction)
+        {
+            count %= (byte)(array.Length * 8);
+            var result = new byte[array.Length];
+            Array.Copy(array, result, array.Length);
+            while(count > 8)
+            {
+                result = PerformCycleShift(result, 8, direction);
+                count -= 8;
+            }
+            result = PerformCycleShift(result, count, direction);
+            return result;
+        }
+        byte[] PerformCycleShift(byte[] array, byte count, Direction direction)
+        {
+                bool[] tempBits;
+                bool[] newTempBits;
+                if (direction == Direction.Left)
+                {
+                    tempBits = SaveBits(array[0], count, direction);
+                    for (int i = array.Length - 1; i >= 0; i--)
+                    {
+                        newTempBits = SaveBits(array[i], count, direction);
+                        array[i] = (byte)(array[i] << count);
+                        PutBits(ref array[i], tempBits, direction);
+                        tempBits = newTempBits;
+                    }
+                }
+                else
+                {
+                    tempBits = SaveBits(array[array.Length - 1], count, direction);
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        newTempBits = SaveBits(array[i], count, direction);
+                        array[i] = (byte)(array[i] >> count);
+                        PutBits(ref array[i], tempBits, direction);
+                        tempBits = newTempBits;
+                    }
+                }
+                return array;
+        }
+        void PutBits(ref byte currentByte, bool[] savedBits, Direction direction)
+        {
+            if (direction == Direction.Left)
+            {
+                Array.Reverse(savedBits);
+                for (int i = 0; i < savedBits.Length; i++)
+                {
+                    SetBit(savedBits[i], ref currentByte, i);
+                }
+            }
+            else
+            {
+                Array.Reverse(savedBits);
+                for (int i = savedBits.Length - 1; i >= 0; i--)
+                {
+                    SetBit(savedBits[i], ref currentByte, 7 - i);
+                }
+            }
+        }
+        bool[] SaveBits(byte currentByte, byte count, Direction direction)
+        {
+            bool[] tempArray = new bool[count];
+            for (uint i = 0; i < count; i++)
+                tempArray[i] = GetBit(currentByte, (direction == Direction.Left) ? (7 - i) : i);
+            return tempArray;
+        }
+        bool GetBit(byte value, uint index)
+        {
+            if (index > 8)
+                throw new Exception("index must be < 8");
+            return ((value & (1 << (int)index)) != 0) ? true : false;
+        }
+        void SetBit(bool bitValue, ref byte container, int index)
+        {
+            container = (byte)((bitValue) ?
+                container | (1 << index) :
+                container & (~(1 << index)));
+        }
+        byte[] GenerateIV(int size)
+        {
+            var result = new byte[size];
+            Random random = new Random();
+            random.NextBytes(result);
+            return result;
+        }
+        byte[] GenerateKey(int size)
+        {
+            var result = new byte[size];
+            Random random = new Random();
+            random.NextBytes(result);
+            return result;
+        }
+        byte[] GetBits(byte[] array, uint count, Direction direction)
+        {
+            if(count > array.Length)
+            {
+                throw new Exception("count must be less that length of array");
+            }
+            var result = new byte[count];
+            for (uint i = 0; i < result.Length; i++)
+                if (direction == Direction.Left)
+                    result[i] = array[i];
+                else
+                    result[i] = array[array.Length-count + i];
+            return result;
+        }
+        byte[] ConcatBits(byte[] array1, byte[] array2)
+        {
+            var result = new byte[array1.Length + array2.Length];
+            Array.Copy(array1, result, array1.Length);
+            Array.Copy(array2, 0, result, array1.Length, array2.Length);
+            return result;
+        }
+    }
+}
---
+++ Feistel/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Feistel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var plainText = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+
+            var f1 = new FeistelCipher(4, FeistelType.ClassicFeistel, CipherMode.None);
+            var cipherText1 = f1.CryptText(plainText, CryptType.Encrypt);
+            var plainTextAgain1 = f1.CryptText(cipherText1, CryptType.Decrypt);
+            var result1 = plainText.Equals(plainTextAgain1);
+            System.Console.WriteLine("Cipher: Classic Feistel. Mode: None\nplain text: {0}\nafter encoding: {1}\nafter decoding: {2}\nequals: {3}\n\n", plainText, cipherText1, plainTextAgain1, result1);
+
+            var f2 = new FeistelCipher(4, FeistelType.Variant1, CipherMode.None);
+            var cipherText2 = f2.CryptText(plainText, CryptType.Encrypt);
+            var plainTextAgain2 = f2.CryptText(cipherText2, CryptType.Decrypt);
+            var result2 = plainText.Equals(plainTextAgain2);
+            System.Console.WriteLine("Cipher: Variant 1. Mode: None\nplain text: {0}\nafter encoding: {1}\nafter decoding: {2}\nequals: {3}\n\n", plainText, cipherText2, plainTextAgain2, result2);
+
+            var f3 = new FeistelCipher(4, FeistelType.Variant1, CipherMode.CBC);
+            var cipherText3 = f3.CryptText(plainText, CryptType.Encrypt);
+            var plainTextAgain3 = f3.CryptText(cipherText3, CryptType.Decrypt);
+            var result3 = plainText.Equals(plainTextAgain3);
+            System.Console.WriteLine("Cipher: Variant 1. Mode: CBC\nplain text: {0}\nafter encoding: {1}\nafter decoding: {2}\nequals: {3}\n\n", plainText, cipherText3, plainTextAgain3, result3);
+
+            var f4 = new FeistelCipher(4, FeistelType.Variant1, CipherMode.CFB);
+            var cipherText4 = f4.CryptText(plainText, CryptType.Encrypt);
+            var plainTextAgain4 = f4.CryptText(cipherText4, CryptType.Decrypt);
+            var result4 = plainText.Equals(plainTextAgain4);
+            System.Console.WriteLine("Cipher: Variant 1. Mode: CFB\nplain text: {0}\nafter encoding: {1}\nafter decoding: {2}\nequals: {3}\n\n", plainText, cipherText4, plainTextAgain4, result4);
+
+            System.Console.ReadKey();
+        }
+    }
+}
